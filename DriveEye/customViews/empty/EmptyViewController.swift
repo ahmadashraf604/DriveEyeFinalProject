1dd795497b13f376559a138390fef89d66f24839
ahmed ashraf <ahmadashraf604@gmail.com>
2019-06-21 15:40:10 +0200

    handle an empty table view

---
+++ DriveEye/customViews/empty/EmptyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  emptyView.swift
+//  DriveEye
+//
+//  Created by ashraf on 6/21/19.
+//  Copyright © 2019 java. All rights reserved.
+//
+
+import UIKit
+
+class emptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
