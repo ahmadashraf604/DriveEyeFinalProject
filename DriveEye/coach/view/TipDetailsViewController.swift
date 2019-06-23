5ef7aafdcc98af4f8f01602f3dfdebc73898dce0
Sallam <sallam@Abanoubs-Mac.local>
2019-06-23 21:39:54 +0200

    added coaching

---
+++ DriveEye/coach/view/TipDetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TipDetailsViewController.swift
+//  DriveEye
+//
+//  Created by Sallam on 6/23/19.
+//  Copyright © 2019 java. All rights reserved.
+//
+
+import UIKit
+
+class TipDetailsViewController: UIViewController {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var tfTitle: UILabel!
+    @IBOutlet weak var tvDescription: UITextView!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBAction func dismissAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    var coachModel: Coach!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stackView.addBackground(color: .white)
+        img.image = coachModel.image
+        tfTitle.text = coachModel.title
+        tvDescription.text = coachModel.responseDescription
+    }
+
+}
